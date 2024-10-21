bdaaf70222715bbe82b49748f5cf185d3cf7c201
candela <candelap.r.p@gmail.com>
2024-10-21 18:46:11 +0200

    [WD-PT-SEP24]- candela rodriguez

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", startCountdown);
 
 
 
@@ -17,22 +18,45 @@ function startCountdown() {
 
 
   // Your code goes here ...
+let timeLeft = 10
+const timerDisplay = document.getElementById("time");
+
+const countdownInterval = setInterval(() => {
+  timeLeft--;
+  timerDisplay.textContent = timeLeft;
+
+  if (timeLeft <= 0) {
+    clearInterval(countdownInterval);
+    startBtn.disabled = false;
+    showToast()
+  }
+}, 1000)
 }
-
-
-
 
 // ITERATION 3: Show Toast
 function showToast(message) {
   console.log("showToast called!");
 
   // Your code goes here ...
+const showToast = document.getElementById("toast");
+showToast.classList.add("show");
 
+setTimeout(() => {
+  showToast.classList.remove("show");
+}, 3000)
 
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
-  // Your code goes here ...
+  // Your code goes here ..
+  
+  if (timeLeft === 10) {
+    showToast("⏰ Final countdown! ⏰");
+  } else if(timeLeft === 5) {
+    showToast("Start the engines! 💥");
+  } else {
+    showToast ("Lift off! 🚀")
+  }
 
 }
